a692c7760bcb8d92830a2347060088fe5c7a2214
Devarajacharya <devrajacharya9860@gmail.com>
2019-12-27 08:30:49 +0530

    deque

---
+++ Collection/src/www/collectionInterface/deque/Deque1.java
@@ -0,0 +1,21 @@
+package www.collectionInterface.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Deque1 {
+	
+	public static void main(String[] args) {
+		Deque<Integer> dq = new ArrayDeque <Integer>();
+		dq.add(34);
+		dq.add(73);
+		dq.add(36);
+		dq.add(45);
+		for (Integer integer : dq) {
+			System.out.println(integer);
+		}
+			
+		
+	}
+
+}
