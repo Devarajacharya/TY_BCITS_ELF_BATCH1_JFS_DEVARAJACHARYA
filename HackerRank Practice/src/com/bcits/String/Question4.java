de64429bf5402d33bf74afd639ce7585401e7f0a
Devarajacharya <devrajacharya9860@gmail.com>
2020-01-01 19:48:35 +0530

    1-1-2020

--- HackerRank Practice/src/com/bcits/String/Question4.java
+++ HackerRank Practice/src/com/bcits/String/Question4.java
@@ -1,45 +1,46 @@
 package com.bcits.String;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Question4 {
 
-	public static ArrayList<String> duplicateWord(ArrayList<String> a1) {
-		for (int i = 0; i < a1.size(); i++) {
-			int index = 0;
-			String[] str = (a1.get(i)).split(" ");
-			String res = "";
-			for (int j = 1; j < str.length; j++) {
-				int k = 0;
-				while (k < j) {
-					if (str[j] == str[k]) {
-						break;
-					} else {
-						k++;
-					}
+	public static String primeNum(HashSet<Integer> arr) {
+		String n1 = "";
+		for (Integer num : arr) {
+			int count = 0;
+			for (int j = 1; j <= num; j++) {
+				if (num % j == 0) {
+					count++;
 				}
-				res += str[j] + " ";
-
-				a1.set(i, res);
 			}
-
+			if (count == 2) {
+				n1 += num + " ";
+			}
 		}
-		return a1;
+		return n1;
+
 	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		ArrayList<String> a1 = new ArrayList<String>();
+		HashSet<Integer> arr = new HashSet<Integer>();
 		System.out.println("Enter the number of Query : ");
 		int num = Integer.parseInt(scan.nextLine());
+		System.out.println("Enter the Integer");
+		ArrayList<String> a1 = new ArrayList<String>();
 		for (int i = 1; i <= num; i++) {
-			System.out.println("Enter the " + i + " String : ");
-			a1.add(scan.nextLine());
+			int n = scan.nextInt();
+			arr.add(n);
+			String res = primeNum(arr);
+			if(res != "") {
+				a1.add(res);
+			}
 		}
-		ArrayList<String> l1 = duplicateWord(a1);
-		for (Object object : l1) {
-			System.out.println(object);
+		System.out.println("The Result is : ");
+		for (String res : a1) {
+			System.out.println(res);
 		}
 		scan.close();
 	}
