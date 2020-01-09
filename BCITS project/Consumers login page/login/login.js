c3e395369126605b32ec23dbbf57dbbdeeda02f2
Devarajacharya <devrajacharya9860@gmail.com>
2020-01-09 10:08:27 +0530

    my project

---
+++ BCITS project/Consumers login page/login/login.js
@@ -0,0 +1,34 @@
+function validation() {
+    var user = document.getElementById("name").value;
+    var email = document.getElementById("emailid").value;
+    var fpsw = document.getElementById("psw").value;
+    var spsw = document.getElementById("cpsw").value;
+
+    if (user == "") {
+        document.getElementById("username").innerHTML = " *Please fill the user name field";
+        return false;
+    } else if ((user.length < 3) || (user.length > 15)) {
+        document.getElementById("username").innerHTML = "*user length must be between 2 and 15";
+        return false;
+    } else if( isNaN(user)){
+        document.getElementById("username").innerHTML ="*only characters are allowed";
+        return false;
+    }else {
+        document.getElementById("username").style.display = 'none';
+    }
+
+
+    if (email == "") {
+        document.getElementById("usermail").innerHTML = " *Please fill the email field";
+        return false;
+    } else {
+        document.getElementById("usermail").style.display = 'none';
+    }
+    if (fpsw == "") {
+        document.getElementById("userpsw").innerHTML = " *Please fill password field";
+        return false;
+    }else{
+        document.getElementById("userpsw").style.display = 'none';
+
+    }
+}
