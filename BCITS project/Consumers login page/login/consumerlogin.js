c5933fa77a428a5e299aef6a4720c51a70c6e784
Devaraj Acharay <devrajacharya9860@gmail.com>
2020-01-18 19:24:04 +0530

    bcits use case

---
+++ BCITS project/Consumers login page/login/consumerlogin.js
@@ -0,0 +1,27 @@
+function validation() {
+    var user = document.getElementById("rrnum").value;
+    var password = document.getElementById("psw").value;
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?a-z]+/;
+    if (user == "") {
+        document.getElementById("username").innerHTML = " *Please fill the user name field";
+        return false;
+    } else if ((user.length < 6) || (user.length > 9)) {
+        document.getElementById("username").innerHTML = "*Invalid RR Number";
+        return false;
+    } else if (format.test(user)){
+        document.getElementById("username").innerHTML = "*Invalid RR Number";
+        return false;
+    } else {
+        document.getElementById("username").style.display = 'none';
+    }   
+
+    if (password == "") {
+        document.getElementById("userpsw").innerHTML = " *Please fill password field";
+        return false;
+    }else{
+        document.getElementById("userpsw").style.display = 'none';
+
+    }
+}
+
+
